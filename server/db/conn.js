4a1af1a73a26caf8181942e1e63cae3adb188276
basannofal <nofalbasan@gmail.com>
2024-09-14 15:09:58 +0530

    develope some changes

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
-// const url = "mongodb+srv://gov:gov@cluster0.1sbl41m.mongodb.net/gov?retryWrites=true&w=majority";
+const url = "mongodb+srv://gov:gov@cluster0.1sbl41m.mongodb.net/gov?retryWrites=true&w=majority";
 
 
-const url = 'mongodb://localhost:27017/Gov';
+// const url = 'mongodb://localhost:27017/Gov';
 
 mongoose.connect(url).then(() => {
     console.log("db connected");
